933bdde3ebd8deb7b383cd1a81509e6214c4f789
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-01-28 16:17:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className=' rounded-2xl bg-amber-500 p-14 m-14'>React App</h1>
+   </div>
+  )
+}
+
+export default App
