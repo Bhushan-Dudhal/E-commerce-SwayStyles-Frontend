fefc1d4166f8d6af31e869f48a48b49081fa56ff
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-02-01 15:25:40 +0530

    HomeSectionCard created cards

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
 
 const HomeSectionCarousel = () => {
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items:5.5 },
+  };
+  
+  const items =[1,1,1,1,1].map((items)=><HomeSectionCard/>)
   return (
-    <div>HomeSectionCarousel</div>
+    <div className='relative px-4 lg:px-8'>
+      <div className='relative p-5'>
+        <AliceCarousel
+                    disableButtonsControls
+                    items={items}
+                    infinite
+                    responsive={responsive}
+          />
+      </div>
+    </div>
   )
 }
 
