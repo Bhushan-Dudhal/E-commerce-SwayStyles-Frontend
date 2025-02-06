ea922b8cab33cdf2b2874b12f2d25f5a20498fe6
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-02-06 15:52:43 +0530

    footer page update

--- src/customer/components/footer/Footer.jsx
+++ src/customer/components/footer/Footer.jsx
@@ -114,7 +114,9 @@ const Footer = () => {
         </Grid>
         <div>
           <h1>
-            
+            <h2>
+              
+            </h2>
           </h1>
         </div>
       </Grid>
